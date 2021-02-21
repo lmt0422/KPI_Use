439edc855a999f408d53787df1aec853bb04b9b0
lmt0001 <lmt25207795@gmail.com>
2021-02-21 15:47:33 +0900

    mapper配置修正，正常运行状态。

--- src/main/java/com/alpinetech/demo/controller/UserController.java
+++ src/main/java/com/alpinetech/demo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserServiceImpl userService;
+    @ResponseBody
     @GetMapping("/getUser")
     public List<User> getUser(){
     return userService.selectAll();
